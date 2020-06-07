de3cfd663b7cdf4f4cf266376f1177883d80c36d
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-06-07 15:15:13 +0200

    style: formatted the code

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -4,38 +4,35 @@ import { NgModuleCompileResult } from '@angular/compiler/src/ng_module_compiler'
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.scss']
+  styleUrls: ['./clock.component.scss'],
 })
 export class ClockComponent {
+  @Input() totalTimeInMs: number;
+  milliseconds: string;
+  seconds: string;
+  minutes: string;
 
-   @Input() totalTimeInMs:number;
-   milliseconds: string;
-   seconds: string;
-   minutes: string;
+  normalizeTime(timeInMs) {
+    const minutes = Math.floor(timeInMs / 60000);
+    const seconds = Math.floor(timeInMs / 1000) % 60;
+    const totalSeconds = (timeInMs / 1000) % 60;
+    const milliseconds = Math.round((totalSeconds - seconds) * 1000);
+    return { minutes, seconds, milliseconds };
+  }
 
-   normalizeTime(timeInMs) {
-      const minutes = Math.floor(timeInMs / 60000);
-      const seconds = Math.floor(timeInMs / 1000) % 60
-      const totalSeconds = (timeInMs / 1000) % 60;
-      const milliseconds = Math.round((totalSeconds - seconds) * 1000);
-      return {minutes, seconds, milliseconds}
-   }
+  formatTime({ minutes, seconds, milliseconds }) {
+    return {
+      minutes: String(minutes).padStart(2, '0'),
+      seconds: String(seconds).padStart(2, '0'),
+      milliseconds: String(milliseconds).padStart(3, '0'),
+    };
+  }
 
-   formatTime({minutes, seconds, milliseconds}) {
-      return {
-         minutes: String(minutes).padStart(2, '0'),
-         seconds: String(seconds).padStart(2, '0'),
-         milliseconds: String(milliseconds).padStart(3, '0')
-
-      }
-   }
-  
-   ngOnInit() {   
-      const normalizedTime = this.normalizeTime(this.totalTimeInMs);
-      const {minutes, seconds, milliseconds}= this.formatTime(normalizedTime);
-      this.minutes = minutes;
-      this.seconds = seconds;
-      this.milliseconds = milliseconds;
-   }
-   
+  ngOnInit() {
+    const normalizedTime = this.normalizeTime(this.totalTimeInMs);
+    const { minutes, seconds, milliseconds } = this.formatTime(normalizedTime);
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.milliseconds = milliseconds;
+  }
 }
