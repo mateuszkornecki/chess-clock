dfc05369eaef8ceb7c0fbc03c3226ba26e605436
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2020-06-13 21:15:06 +0200

    feat: clock-interval now depends on the date time

--- src/app/clock-interval/clock-interval.component.ts
+++ src/app/clock-interval/clock-interval.component.ts
@@ -8,7 +8,13 @@ import { Component, OnDestroy } from '@angular/core';
 export class ClockIntervalComponent {
   timeLeft = 5000;
   isRunning = false;
+  isStarted = false;
   interval;
+  timeToCount = 5000;
+  startTime;
+  actualTime;
+  finishTime;
+  collapsedTime = 0;
 
   toggle(): void {
     this.isRunning = !this.isRunning;
@@ -16,9 +22,23 @@ export class ClockIntervalComponent {
 
   onStart(): void {
     this.toggle();
+    this.startTime = this.actualTime = Date.now();
+    if (!this.isStarted) {
+      this.isStarted = true;
+      this.finishTime = this.startTime + this.timeToCount;
+      this.handleInterval();
+    } else {
+      this.finishTime = this.startTime + this.timeLeft;
+      this.handleInterval();
+    }
+  }
+
+  handleInterval() {
     this.interval = setInterval(() => {
-      if (this.timeLeft > 0) {
-        this.timeLeft -= 10;
+      if (this.actualTime < this.finishTime) {
+        this.actualTime = Date.now();
+        this.collapsedTime += 10;
+        this.timeLeft = this.timeToCount - this.collapsedTime;
       } else {
         clearInterval(this.interval);
       }
