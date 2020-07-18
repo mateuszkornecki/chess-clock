5f33a6a0ed60320f0b926394b2b978f9a94a637c
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-07-18 17:30:10 +0200

    feat: added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerDisplayComponent } from './timer-display/timer-display.component';
 import { TimerIntervalComponent } from './timer-interval/timer-interval.component';
@@ -8,6 +10,11 @@ import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { TimersSynchronizerComponent } from './timers-synchronizer/timers-synchronizer.component';
+
+const routes: Routes = [
+  {path: 'timers', component: TimersSynchronizerComponent},
+  { path: '', redirectTo: '/timers', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +26,9 @@ import { TimersSynchronizerComponent } from './timers-synchronizer/timers-synchr
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
