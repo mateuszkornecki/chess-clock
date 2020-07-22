bf7c4d5dd6daca028ba409a5bc31795db7fbfea8
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2020-07-22 07:46:08 +0200

    feat: updated store initialState to use the value from the localStorage

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,13 +1,14 @@
 import { createReducer, on } from '@ngrx/store';
 import { increment, decrement, reset, custom } from './counter.actions';
 
-export const initialState = 5;
+const valueFromLocalStorage = Number(localStorage.getItem('chess-clock'));
+export const initialState = valueFromLocalStorage ? valueFromLocalStorage : 5;
 
 const _counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),
   on(decrement, (state) => state - 1),
-  on(reset, (state) => 5),
+  on(reset, (state) => initialState),
   on(custom, (state, {value}) => value)
 );
 
